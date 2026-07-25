8b4b41e095e53a69955e09ee7cf0ab353475b701
g-sawicki <sawickigrzegorz66@gmail.com>
2026-07-25 11:32:34 +0200

    Inital commit

---
+++ GEngine/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <Windows.h>
+#include <shellapi.h>
+
+#if defined(CreateWindow)
+#undef CreateWindow
+#endif
+
+#include <wrl.h>
+using namespace Microsoft::WRL;
+
+// DirectX 12 specific headers.
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+// D3D12 extension library.
+//#include <d3dx12.h>
+
+#include <iostream>
+
+int main()
+{
+	std::printf("Hello World");
+	return 0;
+}
